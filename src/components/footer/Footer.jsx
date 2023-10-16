ccf3f6ff9a1ab855a06a6b91a97afaf9133f627b
nimishsara12 <nimishsarathe561@gmail.com>
2023-10-16 14:07:05 +0530

    16oce

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,8 +24,9 @@ const Footer = () => {
                 </ul>
                 <div className="infoText">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    do eiusmod tempor incididunt  abore et dolore magna
+                    aliqua. Ut enim ad
+                    niam, quis nostrud exercitation
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit
                     esse cillum dolore eu fugiat nulla pariatur.
